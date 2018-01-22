fc5ec833a7e0798d2cf039257e684ffec4dacf97
rsquaredin <rsquaredin@gmail.com>
2018-01-22 11:02:20 +0530

    improve output

--- R/ols-stepaic-both-regression.R
+++ R/ols-stepaic-both-regression.R
@@ -1,5 +1,5 @@
 #' @title Stepwise AIC Regression
-#' @description Build regression model from a set of candidate predictor variables by entering and removing predictors based on 
+#' @description Build regression model from a set of candidate predictor variables by entering and removing predictors based on
 #' Akaike Information Criteria, in a stepwise manner until there is no variable left to enter or remove any more.
 #' @param model an object of class \code{lm}
 #' @param x an object of class \code{ols_stepaic_both}
@@ -64,6 +64,14 @@ ols_stepaic_both.default <- function(model, details = FALSE) {
     mo         <- lm(paste(response, '~', 1), data = l)
     aic_c      <- ols_aic(mo)
 
+    cat(format("Stepwise Selection Method", justify = "left", width = 25), "\n")
+    cat(rep("-", 25), sep = "", '\n\n')
+    cat(format("Candidate Terms:", justify = "left", width = 16), "\n\n")
+    for (i in seq_len(length(nam))) {
+      cat(paste(i, ".", nam[i]), "\n")
+    }
+    cat('\n')
+
     if (details == TRUE) {
         cat(' Step 0: AIC =', aic_c, '\n', paste(response, '~', 1, '\n\n'))
     }
@@ -78,6 +86,11 @@ ols_stepaic_both.default <- function(model, details = FALSE) {
     lrss      <- c()
     lrsq      <- c()
     larsq     <- c()
+
+    cat('\n')
+    if (!details) {
+      cat('Variables Entered/Removed:', '\n\n')
+    }
 
     while (step < mlen_p) {
 
@@ -97,6 +110,38 @@ ols_stepaic_both.default <- function(model, details = FALSE) {
             rsq[i]  <- m$rsq
             arsq[i] <- m$adjr
 
+        }
+
+        da  <- data.frame(predictors = predictors, aics = aics, ess = ess, rss = rss, rsq = rsq, arsq = arsq)
+        da2 <- arrange(da, desc(rss))
+
+        if(details == TRUE) {
+
+            w1 <- max(nchar('Predictor'), nchar(predictors))
+            w2 <- 2
+            w3 <- max(nchar('AIC'), nchar(format(round(aics, 3), nsmall = 3)))
+            w4 <- max(nchar('Sum Sq'), nchar(format(round(rss, 3), nsmall = 3)))
+            w5 <- max(nchar('RSS'), nchar(format(round(ess, 3), nsmall = 3)))
+            w6 <- max(nchar('R-Sq'), nchar(format(round(rsq, 3), nsmall = 3)))
+            w7 <- max(nchar('Adj. R-Sq'), nchar(format(round(arsq, 3), nsmall = 3)))
+            w  <- sum(w1, w2, w3, w4, w5, w6, w7, 24)
+            ln <- length(aics)
+
+            cat(fc(crayon::bold$green('  Enter New Variables'), w), sep = "", '\n')
+            cat(rep("-", w), sep = "", '\n')
+            cat(fl('Variable', w1), fs(), fc('DF', w2), fs(), fc('AIC', w3), fs(),
+                fc('Sum Sq', w4), fs(), fc('RSS', w5), fs(), fc('R-Sq', w6), fs(),
+                fc('Adj. R-Sq', w7), '\n')
+            cat(rep("-", w), sep = "", '\n')
+
+            for (i in seq_len(ln)) {
+                cat(fl(da2[i, 1], w1), fs(), fg(1, w2), fs(), fg(format(round(da2[i, 2], 3), nsmall = 3), w3), fs(),
+                fg(format(round(da2[i, 4], 3), nsmall = 3), w4), fs(), fg(format(round(da2[i, 3], 3), nsmall = 3), w5), fs(),
+                fg(format(round(da2[i, 5], 3), nsmall = 3), w6), fs(),
+                fg(format(round(da2[i, 6], 3), nsmall = 3), w7), '\n')
+            }
+
+            cat(rep("-", w), sep = "", '\n\n')
         }
 
 
@@ -124,6 +169,12 @@ ols_stepaic_both.default <- function(model, details = FALSE) {
             lrsq       <- c(lrsq, mrsq)
             larsq      <- c(larsq, marsq)
 
+            if (interactive()) {
+              cat(crayon::green(clisymbols::symbol$tick), crayon::bold(dplyr::last(preds)), '\n')
+            } else {
+              cat(paste('-', dplyr::last(preds), 'added'), '\n')
+            }
+
             if (details == TRUE) {
                 cat('\n\n', 'Step', all_step, ': AIC =', maic, '\n', paste(response, '~', paste(preds, collapse = ' + ')), '\n\n')
             }
@@ -148,6 +199,38 @@ ols_stepaic_both.default <- function(model, details = FALSE) {
 
                 }
 
+                da  <- data.frame(predictors = preds, aics = aics, ess = ess, rss = rss, rsq = rsq, arsq = arsq)
+                da2 <- arrange(da, desc(rss))
+
+                if(details == TRUE) {
+
+                    w1 <- max(nchar('Predictor'), nchar(preds))
+                    w2 <- 2
+                    w3 <- max(nchar('AIC'), nchar(format(round(aics, 3), nsmall = 3)))
+                    w4 <- max(nchar('Sum Sq'), nchar(format(round(rss, 3), nsmall = 3)))
+                    w5 <- max(nchar('RSS'), nchar(format(round(ess, 3), nsmall = 3)))
+                    w6 <- max(nchar('R-Sq'), nchar(format(round(rsq, 3), nsmall = 3)))
+                    w7 <- max(nchar('Adj. R-Sq'), nchar(format(round(arsq, 3), nsmall = 3)))
+                    w  <- sum(w1, w2, w3, w4, w5, w6, w7, 24)
+                    ln <- length(aics)
+
+                    cat(fc(crayon::bold$red('Remove Existing Variables'), w), sep = "", '\n')
+                    cat(rep("-", w), sep = "", '\n')
+                    cat(fl('Variable', w1), fs(), fc('DF', w2), fs(), fc('AIC', w3), fs(),
+                        fc('Sum Sq', w4), fs(), fc('RSS', w5), fs(), fc('R-Sq', w6), fs(),
+                        fc('Adj. R-Sq', w7), '\n')
+                    cat(rep("-", w), sep = "", '\n')
+
+                    for (i in seq_len(ln)) {
+                        cat(fl(da2[i, 1], w1), fs(), fg(1, w2), fs(), fg(format(round(da2[i, 2], 3), nsmall = 3), w3), fs(),
+                        fg(format(round(da2[i, 4], 3), nsmall = 3), w4), fs(), fg(format(round(da2[i, 3], 3), nsmall = 3), w5), fs(),
+                        fg(format(round(da2[i, 5], 3), nsmall = 3), w6), fs(),
+                        fg(format(round(da2[i, 6], 3), nsmall = 3), w7), '\n')
+                    }
+
+                    cat(rep("-", w), sep = "", '\n\n')
+                }
+
 
                 minc2 <- which(aics == min(aics))
 
@@ -168,6 +251,13 @@ ols_stepaic_both.default <- function(model, details = FALSE) {
                     var_index <- c(var_index, preds[minc2])
                     method    <- c(method, tech[2])
                     all_step  <- all_step + 1
+
+                    if (interactive()) {
+                      cat(crayon::red(clisymbols::symbol$cross), crayon::bold(preds[minc2]), '\n')
+                    } else {
+                      cat(paste('-', preds[minc2], 'removed'), '\n')
+                    }
+
                     preds     <- preds[-minc2]
                     lpreds    <- length(preds)
 
@@ -187,19 +277,33 @@ ols_stepaic_both.default <- function(model, details = FALSE) {
 
         } else {
 
-            message(paste("No more variables to be added or removed."))
-            break
+          cat('\n')
+          cat(crayon::bold$red("No more variables to be added or removed."))
+          break
+
 
         }
+
+    }
+
+    if(details == TRUE) {
+
+      cat('\n\n')
+      cat('Final Model Output', '\n')
+      cat(rep("-", 18), sep = "", '\n\n')
+
+      fi <- ols_regress(paste(response, '~', paste(preds, collapse = ' + ')),
+                        data = l)
+      print(fi)
 
     }
 
     out <- list(predictors = var_index,
                 method     = method,
                 aic        = laic,
-                ess        = less, 
-                rss        = lrss, 
-                rsq        = lrsq, 
+                ess        = less,
+                rss        = lrss,
+                rsq        = lrsq,
                 arsq       = larsq,
                 steps      = all_step)
 
