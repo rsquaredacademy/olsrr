c9d1b18f2c084b6f0f2d77ec1ba6b9f677982b75
rsquaredin <rsquaredin@gmail.com>
2018-02-17 23:23:28 +0530

    refactor code

--- R/ols-dffits-plot.R
+++ R/ols-dffits-plot.R
@@ -37,33 +37,49 @@
 #' @export
 #'
 ols_dffits_plot <- function(model) {
+
   if (!all(class(model) == "lm")) {
     stop("Please specify a OLS linear regression model.", call. = FALSE)
   }
 
-  dffitsm <- model %>% dffits() %>% unlist()
-  k <- length(model$coefficients)
-  n <- model %>% model.frame() %>% nrow()
-  dffits_t <- 2 * sqrt(k / n)
   obs <- NULL
   txt <- NULL
   dbetas <- NULL
   Observation <- NULL
 
-  d <- tibble(obs = seq_len(n), dbetas = dffitsm)
-  d$color <- ifelse(((d$dbetas >= dffits_t) | (d$dbetas <= -dffits_t)), c("outlier"), c("normal"))
-  d$color1 <- factor(d$color)
-  d$Observation <- ordered(d$color1, levels = c("normal", "outlier"))
-  d <- d %>% mutate(txt = ifelse(Observation == "outlier", obs, NA))
-  f <- d %>% filter(., Observation == "outlier") %>% select(obs, dbetas)
+  dffitsm <-
+    model %>%
+    dffits() %>%
+    unlist()
+
+  k <- model_n_coeffs(model)
+
+  n <- model_rows(model)
+
+  dffits_t <-
+    k %>%
+    divide_by(n) %>%
+    sqrt() %>%
+    multiply_by(2)
+
+  title <-
+    model %>%
+    model.frame() %>%
+    names() %>%
+    extract(1)
+
+  dfits_data <- tibble(obs = seq_len(n), dbetas = dffitsm)
+  d <- dfb_plot_data(dfits_data, dffits_t)
+  f <- dfb_outlier_data(d)
 
   p <- ggplot(d, aes(x = obs, y = dbetas, label = txt, ymin = 0, ymax = dffitsm)) +
     geom_linerange(colour = "blue") +
     geom_hline(yintercept = c(0, dffits_t, -dffits_t), colour = "red") +
     geom_point(colour = "blue", shape = 1) +
     xlab("Observation") + ylab("DFFITS") +
-    ggtitle(paste("Influence Diagnostics for", names(model.frame(model))[1])) +
-    geom_text(hjust = -0.2, nudge_x = 0.15, size = 3, family = "serif", fontface = "italic", colour = "darkred", na.rm = TRUE) +
+    ggtitle(paste("Influence Diagnostics for", title)) +
+    geom_text(hjust = -0.2, nudge_x = 0.15, size = 3, family = "serif",
+              fontface = "italic", colour = "darkred", na.rm = TRUE) +
     annotate(
       "text", x = Inf, y = Inf, hjust = 1.5, vjust = 2,
       family = "serif", fontface = "italic", colour = "darkred",
@@ -74,4 +90,5 @@ ols_dffits_plot <- function(model) {
   colnames(f) <- c("Observation", "DFFITs")
   result <- list(outliers = f, threshold = round(dffits_t, 2), plot = p)
   invisible(result)
+
 }
