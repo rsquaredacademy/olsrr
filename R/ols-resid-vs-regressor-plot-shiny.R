e9f7b5f83e1fa283c708a7eaaf70580b134bc32b
rsquaredin <rsquaredin@gmail.com>
2018-02-22 11:49:58 +0530

    remove deprecated functions

--- R/ols-resid-vs-regressor-plot-shiny.R
+++ R/ols-resid-vs-regressor-plot-shiny.R
@@ -11,6 +11,7 @@
 #' @export
 #'
 rvsr_plot_shiny <- function(model, data, variable) {
+
   if (!all(class(model) == "lm")) {
     stop("Please specify a OLS linear regression model.", call. = FALSE)
   }
@@ -20,76 +21,21 @@ rvsr_plot_shiny <- function(model, data, variable) {
   d <- rvsrdata(model)
 
   v <- l(deparse(substitute(variable)))
-  xvar <- data %>% select_(variable)
+
+  xvar <-
+    data %>%
+    select(!! sym(variable))
+
   k <- data.frame(x = xvar, y = model$residuals)
   colnames(k) <- c("x", "y")
-  p <- ggplot(k, aes(x = x, y = y))
-  p <- p + geom_point(shape = 1, colour = "blue")
-  p <- p + xlab(paste(variable)) + ylab("Residual")
-  p <- p + ggtitle(paste("Residual vs", variable))
-  p <- p + geom_hline(yintercept = 0, colour = "red")
+
+  p <- ggplot(k, aes(x = x, y = y)) +
+    geom_point(shape = 1, colour = "blue") +
+    xlab(paste(variable)) + ylab("Residual") +
+    ggtitle(paste("Residual vs", variable)) +
+    geom_hline(yintercept = 0, colour = "red")
+
+
   print(p)
 }
 
-# rvsr_plot <- function(model) {
-
-# 	if (!all(class(model) == 'lm')) {
-#     stop('Please specify a OLS linear regression model.', call. = FALSE)
-#   }
-
-# 	x <- NULL
-# 	y <- NULL
-# 	d <- rvsrdata(model)
-
-# 		for (i in seq_len(d$np)) {
-# 				k <- data.frame(x = unlist(d$dat[i]), y = model$residuals)
-# 				p <- ggplot(k, aes(x = x, y = y))
-# 				p <- p + geom_point(shape = 1, colour = 'blue')
-# 				p <- p + xlab(paste(d$pnames[i])) + ylab('Residual')
-# 				p <- p + ggtitle(paste("Residual vs", d$pnames[i]))
-# 				p <- p + geom_hline(yintercept = 0, colour = 'red')
-# 				print(p)
-# 		}
-
-# }
-
-
-# rvsr_plot <- function(model, panel = TRUE) {
-#
-# 	if (!all(class(model) == 'lm')) {
-#     stop('Please specify a OLS linear regression model.', call. = FALSE)
-#   }
-#
-#
-# 	np     <- length(model$coefficients) - 1
-# 	dat    <- model.frame(model)[-1]
-# 	pnames <- names(model$coefficients)[-1]
-# 	dnames <- colnames(model.frame(model))[1]
-#
-# 	if (panel) {
-#
-# 		ncols  <- 2
-# 		nrows  <- ceiling(np / 2 )
-# 		op     <- par(no.readonly = TRUE)
-#
-# 		on.exit(par(op))
-# 		par(mfrow=c(nrows,ncols), oma = c(0, 0, 2, 0))
-# 		for (i in seq_len(np)) {
-# 		    plot(unlist(dat[i]), model$residuals, col = "blue",
-# 		         xlab = paste(pnames[i]), ylab = "Residual")
-# 		    abline(h = 0)
-# 		}
-# 		mtext(paste("Residual by Regressors for", dnames), outer = T)
-#
-# 	} else {
-#
-# 		for (i in seq_len(np)) {
-# 		    plot(unlist(dat[i]), model$residuals, col = "blue",
-# 		         xlab = paste(pnames[i]), ylab = "Residual",
-# 		         main = paste("Residual vs", pnames[i]))
-# 		    abline(h = 0)
-# 		}
-#
-# 	}
-#
-# }
