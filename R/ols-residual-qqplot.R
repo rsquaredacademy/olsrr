9d98b28292279701e50f0ec45253ecc0d0837cf0
rsquaredin <rsquaredin@gmail.com>
2018-02-22 11:50:46 +0530

    clean up code

--- R/ols-residual-qqplot.R
+++ R/ols-residual-qqplot.R
@@ -8,6 +8,7 @@
 #' @export
 #'
 ols_rsd_qqplot <- function(model) {
+
   if (!all(class(model) == "lm")) {
     stop("Please specify a OLS linear regression model.", call. = FALSE)
   }
@@ -18,9 +19,12 @@ ols_rsd_qqplot <- function(model) {
   slope <- diff(y) / diff(x)
   int <- y[1L] - slope * x[1L]
   d <- tibble(x = resid)
+
   p <- ggplot(d, aes(sample = x)) + stat_qq(color = "blue") +
     geom_abline(slope = slope, intercept = int, color = "red") +
     xlab("Theoretical Quantiles") + ylab("Sample Quantiles") +
     ggtitle("Normal Q-Q Plot")
+
   print(p)
+
 }
