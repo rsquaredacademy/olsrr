866004d45bda5fefb2958f183c612d1d7a1e8908
rsquaredin <rsquaredin@gmail.com>
2018-12-24 14:52:42 +0530

    closes #115

--- inst/application/logic/logic_screen.R
+++
@@ -1,8 +0,0 @@
-# output
-output$screen <- renderPrint({
-    screener(filt_data$p)
-})
-
-observeEvent(input$finalok, {
-	updateNavbarPage(session, 'mainpage', selected = 'tab_sample')
-})
--- inst/application/ui/ui_analyze.R
+++
@@ -1,4 +0,0 @@
-navbarMenu('Analyze', icon = icon('search-plus'),
-	source('ui/ui_homes.R', local = TRUE)[[1]],
-  source('ui/ui_mlr.R', local = TRUE)[[1]]
-)
