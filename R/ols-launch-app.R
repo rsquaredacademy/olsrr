ae8138d8662b5718e98a04de9a65bcb069dee683
rsquaredin <rsquaredin@gmail.com>
2020-02-04 17:47:14 +0530

    move xplorerr to suggests

--- R/ols-launch-app.R
+++ R/ols-launch-app.R
@@ -8,21 +8,9 @@
 #'
 ols_launch_app <- function() {
 
-	rlang::inform("`ols_launch_app()` has been soft-deprecated and will be removed in the next release. In future, to launch the app, run the below code:\n 
-	- install.packages('xplorerr')\n - xplorerr::app_linear_regression()\n")
+	message("`ols_launch_app()` has been soft-deprecated and will be removed in the next release. In future, to launch the app, run the below code:\n\n - install.packages('xplorerr')\n - xplorerr::app_linear_regression()\n")
 
-	check_suggests('caret')
-	check_suggests('descriptr')
-	check_suggests('jsonlite')
-	check_suggests('haven')
-	check_suggests('lubridate')
-	check_suggests('readr')
-	check_suggests('readxl')
-	check_suggests('scales')
-	check_suggests('shinyBS')
-	check_suggests('shinycssloaders')
-	check_suggests('shinythemes')
-
+	check_suggests('xplorerr')
 	xplorerr::app_linear_regression()
 }
  
