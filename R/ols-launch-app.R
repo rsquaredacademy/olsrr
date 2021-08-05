f3076ab22a026204065b751041d9a064f33a5d3a
rsquaredin <rsquaredin@gmail.com>
2021-08-05 12:33:02 +0530

    chore: remove deprecation message

--- R/ols-launch-app.R
+++ R/ols-launch-app.R
@@ -8,8 +8,6 @@
 #'
 ols_launch_app <- function() {
 
-	message("`ols_launch_app()` has been soft-deprecated and will be removed in the next release. In future, to launch the app, run the below code:\n\n - install.packages('xplorerr')\n - xplorerr::app_linear_regression()\n")
-
 	check_suggests('xplorerr')
 	xplorerr::app_linear_regression()
 }
