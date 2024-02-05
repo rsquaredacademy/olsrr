0849492d1052345ef54f49e6d31777f576182653
aravindhebbali <hebbali.aravind@gmail.com>
2024-02-05 21:36:59 +0530

    feat: custom threshold

--- R/ols-cooks-d-barplot.R
+++ R/ols-cooks-d-barplot.R
@@ -5,8 +5,11 @@
 #' fitted values of the model.
 #'
 #' @param model An object of class \code{lm}.
-#' @param type An integer between 1 and 5 selecting one of the 6 methods for computing the threshold.
-#' @param print_plot logical; if \code{TRUE}, prints the plot else returns a plot object.
+#' @param type An integer between 1 and 5 selecting one of the 5 methods for
+#'     computing the threshold.
+#' @param threshold Threshold for detecting outliers.
+#' @param print_plot logical; if \code{TRUE}, prints the plot else returns a
+#'     plot object.
 #'
 #' @details
 #' Cook's distance was introduced by American statistician R Dennis Cook in
@@ -51,6 +54,8 @@
 #' @examples
 #' model <- lm(mpg ~ disp + hp + wt, data = mtcars)
 #' ols_plot_cooksd_bar(model)
+#' ols_plot_cooksd_bar(model, type = 4)
+#' ols_plot_cooksd_bar(model, threshold = 0.2)
 #'
 #' @importFrom stats cooks.distance
 #' @importFrom ggplot2 geom_bar coord_flip ylim geom_hline geom_label
@@ -59,15 +64,19 @@
 #'
 #' @export
 #'
-ols_plot_cooksd_bar <- function(model, type = 1, print_plot = TRUE) {
+ols_plot_cooksd_bar <- function(model, type = 1, threshold = NULL, print_plot = TRUE) {
 
   check_model(model)
 
   k <- ols_prep_cdplot_data(model, type)
   d <- ols_prep_outlier_obs(k)
   f <- ols_prep_cdplot_outliers(k)
+  
+  if (is.null(threshold)) {
+      threshold <- k$ts
+  }
 
-  y_max <- max(k$maxx, k$ts)
+  y_max <- max(k$maxx, threshold)
   y_lim <- y_max + (y_max * 0.1)
 
   # geoms
@@ -76,33 +85,33 @@ ols_plot_cooksd_bar <- function(model, type = 1, print_plot = TRUE) {
     geom_bar(width = 0.5, stat = "identity", aes(fill = fct_color)) +
     geom_text(hjust = -0.2, nudge_x = 0.05, size = 2, na.rm = TRUE) +
     geom_hline(yintercept = 0) +
-    geom_hline(yintercept = k$ts, colour = "red") 
+    geom_hline(yintercept = threshold, colour = "red")
 
   # annotations
   p <-
     p +
     annotate("text", x = Inf, y = Inf, hjust = 1.2, vjust = 2,
       family = "serif", fontface = "italic", colour = "darkred",
-      label = paste("Threshold:", round(k$ts, 3))) 
+      label = paste("Threshold:", round(threshold, 3)))
 
   # scales
-  p <- 
+  p <-
     p +
-    scale_fill_manual(values = c("blue", "red")) 
+    scale_fill_manual(values = c("blue", "red"))
 
   # guides
-  p <- 
+  p <-
     p +
     labs(fill = "Observation") +
     xlab("Observation") +
-    ylab("Cook's D") + 
-    ggtitle("Cook's D Bar Plot") + 
-    ylim(0, y_lim) 
+    ylab("Cook's D") +
+    ggtitle("Cook's D Bar Plot") +
+    ylim(0, y_lim)
 
   if (print_plot) {
     suppressWarnings(print(p))
   } else {
-    return(list(plot = p, outliers = f, threshold = k$ts))
+    return(list(plot = p, outliers = f, threshold = threshold))
   }
 
 }
