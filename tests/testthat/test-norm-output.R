cc1ad3b6c38161b029c571af5f1d1b3632ac54db
aravindhebbali <hebbali.aravind@gmail.com>
2024-02-12 15:00:24 +0530

    test: update snapshots

--- tests/testthat/test-norm-output.R
+++ tests/testthat/test-norm-output.R
@@ -8,8 +8,3 @@ test_that("output from ols_corr_test is as expected", {
   expect_equal(round(ols_test_correlation(model), 3), 0.97)
 })
 
-test_that("ols_test_normality returns error messages", {
-  model <- glm(prog ~ female + read + science, data = hsb, family = binomial(link = 'logit'))
-  expect_error(ols_test_normality(hsb$female), "y must be numeric")
-  expect_error(ols_test_normality(model), "Please specify a OLS linear regression model.")
-})
