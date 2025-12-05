526e2ab8e9be3ac0fd72d43f5ba917a1ac62bb08
aravindhebbali <hebbali.aravind@gmail.com>
2025-12-06 00:22:32 +0530

    feat: compare models

--- R/ols-model-performance.R
+++ R/ols-model-performance.R
@@ -64,3 +64,55 @@ ols_model_performance.default <- function(model, ...) {
 print.ols_model_performance <- function(x, ...) {
   print_model_perf(x)
 }
+
+#' Compare model performance
+#'
+#' @description
+#' Compare performance of regression models using multiple metrics.
+#'
+#' @param ... Multiple objects of class \code{lm}.
+#'
+#' @examples
+#' model_1 <- lm(mpg ~ disp + hp, data = mtcars)
+#' model_2 <- lm(mpg ~ disp + hp + wt, data = mtcars)
+#' model_3 <- lm(mpg ~ disp + hp + wt + qsec + + drat, data = mtcars)
+#' ols_compare_model_performance(model_1, model_2, model_3)
+#'
+#' @export
+#'
+ols_compare_model_performance <- function(...) UseMethod("ols_compare_model_performance")
+
+#' @export
+#'
+ols_compare_model_performance.lm <- function(...) {
+
+  models <- list(...)
+  n <- length(models)
+  model_names <- sapply(substitute(list(...)), deparse)[-1]
+  result <- data.frame(matrix(ncol = 9, nrow = 0))
+
+  for (i in seq_len(n)) {
+    result <-
+      rbind(
+        result,
+        unlist(olsrr::ols_model_performance(models[[i]]))[-1]
+      )
+  }
+
+  sigma <- vector(mode = "numeric", length = n)
+  for (i in seq_len(n)) {
+    sigma[i] = summary(models[[i]])$sigma
+  }
+
+  result <- cbind(model_names, result)
+  result <- cbind(result, sigma)
+
+  col_names <- c("name", "r2", "r2_adj", "r2_pred", "aic", "sbc", "sbic",
+    "rmse", "sigma")
+  colnames(result) <- col_names
+
+  class(result) <- c("ols_compare_model_performance", "data.frame")
+
+  return(result)
+}
+
